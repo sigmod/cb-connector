cf485099381f97ce97ea42d101501076ef143f5b
Yingyi Bu <yingyi@couchbase.com>
2015-05-06 12:33:00 -0700

    change class name in JDBC test.

--- src/com/couchbase/connector/jdbc/test/JDBCTest.java
+++ src/com/couchbase/connector/jdbc/test/JDBCTest.java
@@ -20,62 +20,57 @@ import org.junit.Test;
 
 public class JDBCTest {
 
-	private static String jdbcDriver = "com.simba.couchbase.jdbc41.Driver";
-	private static String connectionURL = "jdbc:couchbase://localhost:8093;queryEnabled=1;logLevel=6;logPath=log";
-	private static String username = "Administrator";
-	private static String password = "password";
+    private static String jdbcDriver = "com.simba.couchbase.jdbc4.Driver";
+    private static String connectionURL = "jdbc:couchbase://localhost:8093;queryEnabled=1;logLevel=6;logPath=log";
+    private static String username = "Administrator";
+    private static String password = "password";
 
-	@Test
-	public void test() throws Exception {
-		// Load the JDBC Driver class.
-		Class.forName(jdbcDriver);
-		// Establish a connection using the connection // URL
-		Connection connection = DriverManager.getConnection(connectionURL,
-				username, password);
-		String[] types = { "TABLE" };
-		DatabaseMetaData metadata = connection.getMetaData();
-		ResultSet rs = metadata.getTables(null, null, "%", types);
-		ResultSetMetaData rsmd = rs.getMetaData();
-		int columnsNumber = rsmd.getColumnCount();
-		while (rs.next()) {
-			for (int i = 1; i < columnsNumber; i++) {
-				if (rsmd.getColumnLabel(i).equals("TABLE_NAME")) {
-					String tableName = rs.getString(i);
-					if (tableName.contains("-")
-							|| tableName.contains("default")) {
-						continue;
-					}
-					ResultSet schema = metadata.getColumns(null, null,
-							tableName, null);
-					ResultSetMetaData schemaMetadata = schema.getMetaData();
-					while (schema.next()) {
-						for (int j = 1; j <= schemaMetadata.getColumnCount(); j++) {
-							System.out.print(schemaMetadata.getColumnLabel(j)
-									+ ":" + schema.getString(j) + "|");
-						}
-						System.out.println();
-					}
-					System.out.println();
-				}
-			}
-			System.out.println();
-		}
+    @Test
+    public void test() throws Exception {
+        // Load the JDBC Driver class.
+        Class.forName(jdbcDriver);
+        // Establish a connection using the connection // URL
+        Connection connection = DriverManager.getConnection(connectionURL, username, password);
+        String[] types = { "TABLE" };
+        DatabaseMetaData metadata = connection.getMetaData();
+        ResultSet rs = metadata.getTables(null, null, "%", types);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i < columnsNumber; i++) {
+                if (rsmd.getColumnLabel(i).equals("TABLE_NAME")) {
+                    String tableName = rs.getString(i);
+                    if (tableName.contains("-") || tableName.contains("default")) {
+                        continue;
+                    }
+                    ResultSet schema = metadata.getColumns(null, null, tableName, null);
+                    ResultSetMetaData schemaMetadata = schema.getMetaData();
+                    while (schema.next()) {
+                        for (int j = 1; j <= schemaMetadata.getColumnCount(); j++) {
+                            System.out.print(schemaMetadata.getColumnLabel(j) + ":" + schema.getString(j) + "|");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                }
+            }
+            System.out.println();
+        }
 
-		String query = "select top 1 * from `beer-sample`";
-		Statement stmt = connection.createStatement();
-		rs = stmt.executeQuery(query);
-		rsmd = rs.getMetaData();
-		columnsNumber = rsmd.getColumnCount();
-		int row = 0;
-		while (rs.next()) {
-			for (int i = 1; i <= columnsNumber; i++) {
-				System.out.print(rsmd.getColumnLabel(i) + ":" + rs.getString(i)
-						+ "|");
-			}
-			System.out.println();
-			row++;
-		}
-		System.out.println(row);
-		connection.close();
-	}
+        String query = "select top 1 * from `beer-sample`";
+        Statement stmt = connection.createStatement();
+        rs = stmt.executeQuery(query);
+        rsmd = rs.getMetaData();
+        columnsNumber = rsmd.getColumnCount();
+        int row = 0;
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                System.out.print(rsmd.getColumnLabel(i) + ":" + rs.getString(i) + "|");
+            }
+            System.out.println();
+            row++;
+        }
+        System.out.println(row);
+        connection.close();
+    }
 }
